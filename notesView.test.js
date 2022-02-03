8e055c0f89c1c783c445993a365cbfbea1637eaf
Jason Humbert <jasonhumbert@outlook.com>
2022-02-03 12:09:12 +0000

    Allow user to add a new note

--- notesView.test.js
+++ notesView.test.js
@@ -6,17 +6,32 @@ const fs = require('fs');
 const NotesModel = require('./notesModel');
 const NotesView = require('./notesView');
 
+let model;
+let view;
+beforeEach(() => {
+  document.body.innerHTML = fs.readFileSync('./index.html');
+  model = new NotesModel();
+  view = new NotesView(model);
+})
+
 describe('Notes view', () => {
   it('displays 2 notes', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-
-    const model = new NotesModel();
-    const view = new NotesView(model);
     model.addNote('Note one');
     model.addNote('Note two');
 
     view.displayNotes();
 
     expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
+
+  it('adds a new note', () => {
+    const input = document.querySelector('#add-note-input');
+    input.value = 'Note one';
+
+    const button = document.querySelector('#add-note-button');
+    button.click();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].innerText).toEqual('Note one');
   });
 });
